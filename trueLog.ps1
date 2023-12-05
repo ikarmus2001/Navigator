40d35b8890467fc18ac9433754dde3ce96da4ff3
Kacper Hałaczkiewicz <54312917+ikarmus2001@users.noreply.github.com>
2023-12-05 13:40:42 +0100

    direct log execution

--- trueLog.ps1
+++ trueLog.ps1
@@ -2,6 +2,6 @@ param (
     [string]$inputText
 )
 
-Write-Output "Input received: output $inputText"
-echo "Input received: echo $inputText"
 Write-Host "Input received: host $inputText"
+echo "Input received: echo $inputText"
+Write-Output "Input received: output $inputText"
